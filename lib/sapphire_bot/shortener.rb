a194495386f69fa3d5dc00ea9680744ea75b07c2
Kristupas Savickas <savickas.kristupas@gmail.com>
2016-04-24 20:29:06 +0300

    Add ignored urls

--- lib/sapphire_bot/shortener.rb
+++ lib/sapphire_bot/shortener.rb
@@ -2,7 +2,9 @@ require 'google_url_shortener'
 
 module SapphireBot
   class Shortener
+    include StoreData
     def initialize
+      @ignored_urls = load_file("#{Dir.pwd}/data/ignored_urls.yml")
       Google::UrlShortener::Base.api_key = CONFIG[:googl_api]
       LOGGER.info 'google url shortener authorized'
     rescue => e
@@ -11,7 +13,10 @@ module SapphireBot
 
     def shorten(url, bot)
       bot.stats.stats_hash[:urls_shortened] += 1
-      Google::UrlShortener.shorten!(url)
+      unless @ignored_urls.any? { |ignored_url| url.include?(ignored_url) }
+        return Google::UrlShortener.shorten!(url)
+      end
+      url
     end
   end
 end
