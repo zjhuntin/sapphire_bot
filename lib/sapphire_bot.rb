f89ffc7dea47b55f9601c8dbb1c622d0eb5b5945
Kristupas Savickas <savickas.kristupas@gmail.com>
2016-04-24 15:29:22 +0300

    Move some files out of the main directory

--- lib/sapphire_bot.rb
+++ lib/sapphire_bot.rb
@@ -6,19 +6,18 @@ require_relative 'sapphire_bot/logger'
 Discordrb::LOGGER = LOGGER = SapphireBot::Logger.new
 
 require_relative 'sapphire_bot/version'
-
-require_relative 'sapphire_bot/store_data'
+require_relative 'sapphire_bot/other/store_data'
+require_relative 'sapphire_bot/other/helpers'
+require_relative 'sapphire_bot/other/shorten_text'
 
 require_relative 'sapphire_bot/config'
 require_relative 'sapphire_bot/server_config'
 
-require_relative 'sapphire_bot/helpers'
 require_relative 'sapphire_bot/shortener'
-require_relative 'sapphire_bot/shorten_text'
 require_relative 'sapphire_bot/stats'
+require_relative 'sapphire_bot/bot'
 
 Dir["#{File.dirname(__FILE__)}/sapphire_bot/commands/*.rb"].each { |file| require file }
 Dir["#{File.dirname(__FILE__)}/sapphire_bot/events/*.rb"].each { |file| require file }
 
-require_relative 'sapphire_bot/bot'
 require_relative 'sapphire_bot/base'
