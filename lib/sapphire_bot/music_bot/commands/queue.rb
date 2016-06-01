a58f319da80a01b07f32edf56cc5212af48758e3
Kristupas Savickas <savickas.kristupas@gmail.com>
2016-06-01 22:03:14 +0300

    Fix queue command

--- lib/sapphire_bot/music_bot/commands/queue.rb
+++ lib/sapphire_bot/music_bot/commands/queue.rb
@@ -5,9 +5,9 @@ module SapphireBot
         extend Discordrb::Commands::CommandContainer
         command(:queue, description: 'Displays current queue.') do |event|
           id = event.server.id
-          if event.voice && MusicBot.servers.key?(id)
+          if MusicBot.servers.key?(id)
             server = MusicBot.servers[id]
-            event << if !server.queue.empty?
+            event << if server.queue && !server.queue.empty?
                        "`#{server.table}`"
                      else
                        'Queue is empty, use `add` to add more songs.'
