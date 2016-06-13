2b24597e79a74d182ac120ef0acad2238f084def
Kristupas Savickas <savickas.kristupas@gmail.com>
2016-06-13 20:47:54 +0300

    Fix ping command

--- lib/sapphire_bot/commands/ping.rb
+++ lib/sapphire_bot/commands/ping.rb
@@ -3,7 +3,7 @@ module SapphireBot
     module Ping
       extend Discordrb::Commands::CommandContainer
       command(:ping, description: 'Responds with response time') do |event|
-        "#{(Time.now - event.timestamp) * 1000).to_i}ms."
+        "#{((Time.now - event.timestamp) * 1000).to_i}ms."
       end
     end
   end
