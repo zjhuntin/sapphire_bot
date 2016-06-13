bd507e918542c9adb23fae1f4c51fbc8e5b069cf
Kristupas Savickas <savickas.kristupas@gmail.com>
2016-06-13 16:41:20 +0300

    Refactor ping

--- lib/sapphire_bot/commands/ping.rb
+++ lib/sapphire_bot/commands/ping.rb
@@ -2,8 +2,8 @@ module SapphireBot
   module Commands
     module Ping
       extend Discordrb::Commands::CommandContainer
-      command(:ping, description: 'Responds with respond time') do |event|
-        event << "#{((Time.now - event.timestamp) * 1000).to_i}ms."
+      command(:ping, description: 'Responds with response time') do |event|
+        "#{(Time.now - event.timestamp) * 1000).to_i}ms."
       end
     end
   end
