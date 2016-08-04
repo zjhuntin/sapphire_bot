06f2dc47053ae3598f4204b240e6899f256bc467
Kristupas <savickas.kristupas@gmail.com>
2016-08-05 01:40:10 +0300

    Move commands and events to separate file. Thanks to https://github.com/megumisonoda/SaberBot for the idea.

--- lib/sapphire_bot/base.rb
+++ lib/sapphire_bot/base.rb
@@ -7,39 +7,8 @@ module SapphireBot
   GOOGLE = GoogleServices.new
   STATS = Stats.new
 
-  BOT.include! Commands::Announce
-  BOT.include! Commands::Delete
-  BOT.include! Commands::Flip
-  BOT.include! Commands::Invite
-  BOT.include! Commands::Lmgtfy
-  BOT.include! Commands::Roll
-  BOT.include! Commands::Stats
-  BOT.include! Commands::Ping
-  BOT.include! Commands::KickAll
-  BOT.include! Commands::About
-  BOT.include! Commands::Avatar
-  BOT.include! Commands::Eval
-  BOT.include! Commands::Toggle
-  BOT.include! Commands::Set
-  BOT.include! Commands::Default
-  BOT.include! Commands::Settings
-  BOT.include! Commands::Game
-  BOT.include! Commands::Ignore
-  BOT.include! Commands::YoutubeSearch
-  BOT.include! Commands::MusicHelp
-  BOT.include! Commands::Join
-  BOT.include! Commands::Leave
-  BOT.include! Commands::Add
-  BOT.include! Commands::Queue
-  BOT.include! Commands::ClearQueue
-  BOT.include! Commands::Skip
-  BOT.include! Commands::Repeat
-
-  BOT.include! Events::Mention
-  BOT.include! Events::MessagesReadStat
-  BOT.include! Events::AutoShorten
-  BOT.include! Events::MassMessage
-  BOT.include! Events::ReadyMessage
+  Commands.include!
+  Events.include!
 
   at_exit do
     LOGGER.info "Saving files and deleting songs before exiting..."
--- lib/sapphire_bot/commands.rb
+++ lib/sapphire_bot/commands.rb
@@ -1,6 +1,43 @@
 module SapphireBot
   # Module for sapphire commands.
   module Commands
+    # Require files from directory
     Dir["#{File.dirname(__FILE__)}/commands/*.rb"].each { |file| require file }
+
+    @commands = [
+      Announce,
+      Delete,
+      Flip,
+      Invite,
+      Lmgtfy,
+      Roll,
+      Stats,
+      Ping,
+      KickAll,
+      About,
+      Avatar,
+      Eval,
+      Toggle,
+      Set,
+      Default,
+      Settings,
+      Game,
+      Ignore,
+      YoutubeSearch,
+      MusicHelp,
+      Join,
+      Leave,
+      Add,
+      Queue,
+      ClearQueue,
+      Skip,
+      Repeat
+    ]
+
+    def self.include!
+      @commands.each do | command |
+        SapphireBot::BOT.include!(command)
+      end
+    end
   end
 end
